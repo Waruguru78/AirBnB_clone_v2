22bc215b5130d0ef0c3d7e16234708038d34166e
Elizabeth Wangui <92116777+Waruguru78@users.noreply.github.com>
2022-09-17 20:54:09 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" 
+    State Module for HBNB project 
+"""
 
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey, Table
+from sqlalchemy.orm import relationship
+import os
+from models.place import place_amenity
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
+"""
+Amenities module for implementation.
+"""
+ __tablename__ = 'amenities'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship(
+            "Place", secondary=place_amenity)
+    else:
     name = ""
